8757f244a583438fecc6fdebb8ee1098e9a770da
rodri <rodriwachas@gmail.com>
2022-09-08 19:16:42 -0300

    Backend v1

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -19,41 +19,59 @@ const { arrayPerros } = require("./perros.js");
 
 router.get('/dogs', async (req, res) => {
     const perros = await arrayPerros;
+    const perrosDb = await Dog.findAll();
+    let dogsFiltered = [];
+    for (let i = 0; i < perros.length; i++) {
+        dogsFiltered.push({
+            id: perros[i].id,
+            name: perros[i].name,
+            temperament: perros[i].temperament,
+            weight: perros[i].weight,
+            image: perros[i].image
+        })
+    }
+
+    for (let i = 0; i < perrosDb.length; i++) {
+        dogsFiltered.push(perrosDb[i]);
+    }
 
     if(req.query.hasOwnProperty("name")) {
             let nombre = req.query;
-            let dogs = perros.filter(dog => dog.name === nombre.name)
+            let dogs = dogsFiltered.filter(dog => dog.name === nombre.name)
             return res.json(dogs)
-
-    }else{
-        let dogsFiltered = [];
-        for (let i = 0; i < perros.length; i++) {
-            dogsFiltered.push({
-                image: perros[i].image,
-                name: perros[i].name,
-                temperament: perros[i].temperament,
-                weight: perros[i].weight
-            })
-        }
-          return res.json(dogsFiltered);
-    }
+    }else return res.json(dogsFiltered);
 });
 
 router.get("/dogs/:idRaza", async (req, res) => {
     let id = req.params; // {idRaza: ""}
-
+    let dbOrNot = req.query;
     const perros = await arrayPerros;
+    const perrosDb = await Dog.findAll();
 
-    let filteredDogs = perros.filter(d => d.id == id.idRaza)
-    let filteredDogs2 = {
-        image: filteredDogs[0].image,
-        name: filteredDogs[0].name,
-        temperament: filteredDogs[0].temperament,
-        weight: filteredDogs[0].weight,
-        height: filteredDogs[0].height,
-        life_span: filteredDogs[0].life_span,
+    if(dbOrNot.db == "y") {
+        let filteredDogs = perrosDb.filter(d => d.dbId == id.idRaza);
+        let filteredDogs2 = {
+            image: filteredDogs[0].image,
+            name: filteredDogs[0].name,
+            temperament: filteredDogs[0].temperament,
+            weight: filteredDogs[0].weight,
+            height: filteredDogs[0].height,
+            life_span: filteredDogs[0].life_span,
+        }
+        return res.json(filteredDogs2);
+    }else{
+        let filteredDogs = perros.filter(d => d.id == id.idRaza)
+        let filteredDogs2 = {
+            image: filteredDogs[0].image,
+            name: filteredDogs[0].name,
+            temperament: filteredDogs[0].temperament,
+            weight: filteredDogs[0].weight,
+            height: filteredDogs[0].height,
+            life_span: filteredDogs[0].life_span,
+        }
+        return res.json(filteredDogs2);
     }
-    res.json(filteredDogs2);
+
 })
 
 router.get("/temperaments", async (req, res) => {
@@ -64,10 +82,9 @@ router.get("/temperaments", async (req, res) => {
             defaults: {name : t}
             })
             )
-        return xd;
+        return res.json(temperamentos);
     })
     .catch(e => console.log(e));
-    res.json("hola");
 })
 
 // ---------------- POSTS ----------------------
