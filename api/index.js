ec275fe519328addce4cf2a6f7410ab72871de60
rodri <rodriwachas@gmail.com>
2022-10-06 17:56:44 -0300

    prueba Railway

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: "./.env"});
+
 //                       _oo0oo_
 //                      o8888888o
 //                      88" . "88
@@ -17,12 +19,18 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+const PORT = process.env.DB_PORT;
+
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(PORT)
   });
 });
